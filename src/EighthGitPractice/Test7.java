e74f02b5b6b8b17fd8d110a7a7a4e353135120cf
mreshta <mreshta10@gmail.com>
2023-08-15 15:37:37 -0700

    The Aurora eight practice test8

---
+++ src/EighthGitPractice/Test7.java
@@ -0,0 +1,15 @@
+package EighthGitPractice;
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+		System.out.println("This is the test ");
+	}
+
+}
