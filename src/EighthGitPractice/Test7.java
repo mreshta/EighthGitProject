6e3af617dc61b55d994d89e423febdd7bbce2101
mreshta <mreshta10@gmail.com>
2023-08-15 15:29:32 -0700

    The eighth practice test 7

---
+++ src/EighthGitPractice/Test7.java
@@ -0,0 +1,15 @@
+package EighthGitPractice;
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+		System.out.println("This is the test 7");
+	}
+
+}
